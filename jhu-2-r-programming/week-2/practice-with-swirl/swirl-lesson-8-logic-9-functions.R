d37aac5b03fbd95ac26342ac992cffc49f33e5d5
Vu The Doan <doanthevu1910@gmail.com>
2020-05-26 10:32:22 +0200

    Add files via upload

---
+++ jhu-2-r-programming/week-2/practice-with-swirl/swirl-lesson-8-logic-9-functions.R
@@ -0,0 +1,123 @@
+#lesson 8 - logics
+
+swirl()
+
+library(swirl)
+
+rm(list = ls())
+
+swirl()
+
+Vu
+
+1
+
+8
+
+TRUE == TRUE
+
+(FALSE == TRUE) == FALSE
+
+6 == 7
+
+6 < 7
+
+10 <= 10
+
+3
+
+4
+
+5 != 7
+
+(5 != 7) == !TRUE
+
+Vu
+1
+
+5 == 7 == !TRUE
+
+skip()
+
+!5 == 7
+
+1
+
+4
+
+3
+
+2
+
+3
+
+1
+
+4
+
+skip()
+
+1
+1
+
+FALSE & FALSE
+
+TRUE && c(TRUE, FALSE, FALSE)
+
+TRUE || c(TRUE, FALSE, FALSE)
+
+5 > 8 || 6 != 8 && 4 > 3.9
+
+1
+1
+3
+
+isTRUE(6 > 4)
+
+2
+
+identical("twins", "twins")
+
+2
+
+xor(5 == 6, !FALSE)
+
+1
+
+2
+
+ints <- sample(10)
+
+ints
+
+ints > 5
+
+which(c(TRUE, FALSE, TRUE))
+
+which(ints > 7)
+
+3
+
+any(ints < 0)
+
+all(ints > 0)
+
+4
+
+2
+
+doanthevu1910@gmail.com
+
+zoA4b6zo8KKzFbYE
+
+1
+
+9
+
+#lesson 9 - functions
+
+Sys.Date()
+
+mean(c(2, 4, 5))
+
+
